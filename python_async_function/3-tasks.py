4bfc0a3e509c183927b6d8e9b7ffa540ce791b05
Marirosa <arqmvilcherrez@gmail.com>
2023-09-30 15:43:24 -0700

    Tasks to asyncio

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Execute task with async"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    "Function takes a integer and return an asyncio.Task"
+    loop = asyncio.get_event_loop()
+    return loop.create_task(wait_random(max_delay))
