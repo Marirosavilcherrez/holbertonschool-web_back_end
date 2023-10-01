46a9db997c3123628672582a6c1c2d9f1b42475d
Marirosa <arqmvilcherrez@gmail.com>
2023-09-30 19:21:39 -0700

    Async Generator comprehension

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Async Generator"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    "Loop take 10 times and generate a random number"
+    for i in range(10):
+        await asyncio.sleep(1)
+        random_float = random.uniform(0, 10)
+        yield random_float
+
+
+async def main():
+    "Print the time it takes each"
+    async for number in async_generator():
+        print(f"{number}")
+
+
+if __name__ == '__main__':
+    bucle_eventos = asyncio.get_event_loop()
+    try:
+        bucle_eventos.run_until_complete(main())
+    finally:
+        bucle_eventos.close()
