dbec330840f4230bcddd05e47c7a81e73c344134
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-03 20:53:33 -0700

    List all documents mongo in python

---
+++ NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""List all documents in a collection"""
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    result = mongo_collection.find({})
+    new_list = []
+    for document in result:
+        new_list.append(document)
+    return new_list
