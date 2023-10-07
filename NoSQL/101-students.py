dee8405f0a936aa25c0ae0d13a0b4b6906f10b56
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-06 17:39:35 -0700

    List top students

---
+++ NoSQL/101-students.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Function that return all students sorted by average score"""
+
+import pymongo
+
+
+def top_students(mongo_collection):
+    "Return all students in order"
+    pipeline = [
+        {
+            '$group': {
+                '_id': '$_id',
+                'averageScore': {'$avg': '$scores.score'}
+            }
+        },
+        {
+            '$sort': {'averageScore': -1}
+        }
+    ]
+    new_research = mongo_collection.aggregate(pipeline)
+    return new_research
