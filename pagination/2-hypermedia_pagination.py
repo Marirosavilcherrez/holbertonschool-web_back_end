f1e76bdfa87efbd2572bbd52bb71bdcbbb351924
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-08 13:22:50 -0700

    Fix pycodestyle

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -37,11 +37,11 @@ class Server:
         assert isinstance(page_size, int) and page_size > 0
         start_index, end_index = self.index_range(page, page_size)
         return self.dataset()[start_index:end_index]
-    
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         "Function return hypermedia pagination"
         self.dataset()
-        total_pages = (len(self.__dataset) + page_size -1)// page_size 
+        total_pages = (len(self.__dataset) + page_size - 1) // page_size
         data = self.get_page(page, page_size)
 
         response = {
@@ -50,6 +50,6 @@ class Server:
             'data': data,
             'next_page': page + 1 if page < total_pages else None,
             'prev_page': page - 1 if page > 1 else None,
-            'total_pages' : total_pages
+            'total_pages': total_pages
         }
         return response
