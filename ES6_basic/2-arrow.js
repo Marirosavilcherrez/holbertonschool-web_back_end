d381aa734d2542dfb22af48c4629c7cf2bce40f2
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-18 23:03:04 +0000

    Rewrite the code to standard function ES6 arrow syntax

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+/* Rewrite standard function ES6 arrow syntax */
+
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
