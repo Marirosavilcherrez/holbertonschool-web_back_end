ce2db5694931f13d026013cc97dfe0a3f37bbe98
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-18 22:05:46 +0000

    Modify the variables inside the function

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,8 @@
 /* Change variable var to const or let */
 
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
-
-  if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
-  }
+  const task = false;
+  const task2 = true;
 
   return [task, task2];
 }
