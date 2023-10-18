d033d5bd997f1f32b25ff868582c10357c722605
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-18 18:52:22 +0000

    Fix eslint errors

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -4,13 +4,13 @@ export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
-  
+
 export function getLast() {
   return ' is okay';
 }
-  
+
 export function taskNext() {
   let combination = 'But sometimes let';
-  combination += getLast()
+  combination += getLast();
   return combination;
 }
