2c430356db192b0e77476f0b0761fd98773b81c4
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-18 20:47:07 +0000

    Fix error output

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 /* Change variable var to const or let */
 
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
