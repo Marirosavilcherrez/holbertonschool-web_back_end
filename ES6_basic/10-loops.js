09a8ef52fb1eb607d68379b1e8c878b44e8989a8
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-19 05:27:36 +0000

    Fix error from lint

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -4,7 +4,6 @@ export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const idx of array) {
     newArray.push(appendString + idx);
-}
-  
+  }
   return newArray;
 }
