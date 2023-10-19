421bed1e783f80be23068e2fe0098a3ddbef6c1c
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-19 03:52:50 +0000

    Function to use ES6 computed property names

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,14 @@
+/* Function to use ES6 computed property names */
+
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+  budget[`income-${getCurrentYear()}`] = income;
+  budget[`gdp-${getCurrentYear()}`] = gdp;
+  budget[`capita-${getCurrentYear()}`] = capita;
+  return budget;
+}
