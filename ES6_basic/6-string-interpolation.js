c2ab4ccced9c70c258f9f1e8e406031e23e216cf
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-19 01:32:55 +0000

    Fix correct output

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -11,5 +11,6 @@ export default function getSanFranciscoDescription() {
   // Destructure to have access to the elements
   const { income } = budget;
 
-  return `As of ${year} it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+  return `As of ${year} it was the seventh-highest income county in the United States, with a per capita personal income of ${income}.   
+As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
