3c7a29c33dd47fce5b452f48bc45522e0aeab630
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-19 05:25:14 +0000

    Function to use ES6 for...of operator

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/* USe ES6 's for...of operator */
+
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
+}
+  
+  return newArray;
+}
