11750c548357785c4911ec8de4f4ef35e3d8d2d5
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-19 21:14:39 +0000

    Fuction that receive 2 argumnets

---
+++ ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,10 @@
+/* Function that will recive 2 arguments */
+
+export default function createEmployeesObject(departmentName, employees) {
+  const departamentObject = {
+    [departmentName]: {
+      employees,
+    },
+  };
+  return (departamentObject);
+}
