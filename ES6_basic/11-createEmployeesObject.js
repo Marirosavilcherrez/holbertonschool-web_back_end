1a992a90f679ed2b3c1dcc83634b02161850d6e1
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-10-19 16:50:21 -0500

    Change structure

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,10 +1,8 @@
 /* Function that will recive 2 arguments */
 
 export default function createEmployeesObject(departmentName, employees) {
-  const departamentObject = {
-    [departmentName]: {
-      employees,
-    },
+  const departmentObject = {
+    [departmentName]: employees,
   };
-  return departamentObject;
+  return departmentObject;
 }
