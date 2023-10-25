05a96772f2b4744149d1903556001a38054acbf9
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-25 23:53:38 +0000

    Return a promise the parameter is a boolean

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+/* Return a promise, the paramter is a boolean */
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      const newObject = {
+        status: 200,
+        body: 'Success'
+      }
+      resolve(newObject)
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  })
+}
