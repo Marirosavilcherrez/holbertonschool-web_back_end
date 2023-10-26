2d5c2f5ab44d060ecea9c6243474a6a47bd3ee04
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-26 00:54:33 +0000

    Append three handlers to th function

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+/* Append three handles to the function */
+
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      const newObject = {
+        status: 200,
+        body: 'success',
+      }
+      resolve(newObject);
+      console.log('Got a response from the API');
+    } else {
+      reject(new Error());
+      console.log('Got a response from the API');
+    }
+  });
+}
