8f5b89a01713cfaf74a672e84937879410cfa40c
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-26 19:16:18 +0000

    Handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+/* The function should call two function and return an array */
+
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  // call the two functions and return promise
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then(([firstResult, secondResult])=> {
+    // configure the arrays
+      return [
+        { status: 'fulfilled', value: firstResult }, // fulfilled: completado
+        { status: 'fulfilled', value: secondResult }
+      ];
+    })
+    .catch((error) => {
+      return [{ status: 'rejected', value: error }];
+    });
+}
