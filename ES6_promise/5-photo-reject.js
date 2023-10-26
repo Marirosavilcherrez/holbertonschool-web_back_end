5f623e7e3e3dfb7699268b288857d8d3548c0249
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-26 04:52:01 +0000

    Fix lint errors

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,11 +1,11 @@
 /* Function should return a Promise rejecting with an Error and string */
 
 export default function uploadPhoto(filename) {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (filename) {
       reject(new Error(`${filename} cannot be processed`));
     } else {
       resolve('File found');
     }
   });
-} 
+}
