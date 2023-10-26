7f6e7b0bd98bd5dfc1ebbc4c27e5b92cbacbed41
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-10-25 21:26:39 -0500

    Fix manage error

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,17 +1,17 @@
 /* Append three handles to the function */
 
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      const newObject = {
+  return promise
+    .then(() => {
+      const response = {
         status: 200,
         body: 'success',
-      }
-      resolve(newObject);
+      };
       console.log('Got a response from the API');
-    } else {
-      reject(new Error());
+      return response;
+    })
+    .catch(() => {
       console.error('Got a response from the API');
-    }
-  });
+      return new Error();
+    });
 }
