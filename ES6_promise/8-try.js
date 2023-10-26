81a7f98ee68827862da20e25b621e3a64dcf6210
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-26 22:37:40 +0000

    Throw error / try catch

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+/* Write a function that will accept two arguments */
+
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0')
+  } else {
+    return (numerator / denominator)
+  }
+}
