670bffad809f590b8f473de61e48a384cc9c0dc6
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-26 23:38:58 +0000

    Throw an error

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+/* Function should create and return an array */
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
