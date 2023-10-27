7c7edb38e91f6c456b7bd8ff546088fb42069e8b
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-10-27 16:24:10 -0500

    Fix elint error

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -3,7 +3,7 @@
 export default function getListStudentIds(getListStudents) {
   if (!Array.isArray(getListStudents)) {
     return [];
-  } 
+  }
   const ids = getListStudents.map((student) => student.id); // find id
   return ids;
 }
