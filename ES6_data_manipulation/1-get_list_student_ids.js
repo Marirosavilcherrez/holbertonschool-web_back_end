60d4f1056cb8e5a9cef1d4f89dff966a165a4cd4
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-27 21:21:08 +0000

    Function that return an array of ids from a list of object

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+/* Function that return an array of ids from a list of object */
+
+export default function getListStudentIds(getListStudents) {
+  if (!Array.isArray(getListStudents)) {
+    return [];
+  } 
+  const ids = getListStudents.map((student) => student.id); // find id
+  return ids;
+}
