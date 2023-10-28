798fb6ba03227eec5e83c2cf9f8c7f26d51ff176
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-10-28 06:03:49 +0000

    Map data structure

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+/* Function that return a map of groceries */
+
+export default function groceriesList() {
+  const miMapa = new Map();
+  miMapa.set('Apples', 10);
+  miMapa.set('Tomatoes', 10);
+  miMapa.set('Pasta', 1);
+  miMapa.set('Rice', 1);
+  miMapa.set('Banana', 5);
+  return (miMapa);
+}
