128309039a7cc1fed63fe6aa4658a9172d813bea
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-10-28 00:39:36 -0500

    Fix test if pass a empty element

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,15 @@
 /* Function that return a string of all set values */
 
 export default function cleanSet(inputSet, startString) {
+  // Ensure startString is a string; if not, return an empty string
+  if (typeof startString !== 'string') {
+    return '';
+  }
   // Initialize an empty array to store filtered values
   const filteredValues = [];
   // Iterate through each element in the set
   for (const item of inputSet) {
-    if (typeof item === 'string' && startString && item.startsWith(startString)) {
+    if (typeof item === 'string' && item.startsWith(startString) && item !== '') {
       filteredValues.push(item.slice(startString.length));
     }
   }
